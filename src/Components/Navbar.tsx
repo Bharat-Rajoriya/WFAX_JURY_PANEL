ce3eeb3f81e6d37aec79ad1e15f496cce7968389
Matebul1234 <113576114+Matebul1234@users.noreply.github.com>
2024-10-30 17:07:22 +0530

    added table in the jury panel

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -6,7 +6,7 @@ import { faUserTie , faThumbsUp} from '@fortawesome/free-solid-svg-icons';
 const Navbar = () => {
   return (
     <>
-    <div className="main_container">
+    <div className="main_container_navbar">
       <div className="marquee_background">
             <div className="marquee" onMouseOver={(e) => e.currentTarget.classList.add("paused")} onMouseOut={(e) => e.currentTarget.classList.remove("paused")}>
               <b> Last date of submission the score is 31st Nov</b>
@@ -16,8 +16,8 @@ const Navbar = () => {
           <a className="navbar-brand" href="#">
             <img src="/Images/logo.webp" alt="logo" height="80px" />
             <div className="login_box">
-            <FontAwesomeIcon icon={faUserTie} style={{color: "#ffffff",}} />
-            <FontAwesomeIcon icon={faThumbsUp} style={{color:'white'}}/>
+            {/* <FontAwesomeIcon icon={faUserTie} style={{color: "#ffffff",}} />
+            <FontAwesomeIcon icon={faThumbsUp} style={{color:'white'}}/> */}
               
             </div>
           </a>
