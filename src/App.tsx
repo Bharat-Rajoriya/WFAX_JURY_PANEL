fcb34e5297e889ef0053209aeab85737cf14278e
Bharat-Rajoriya <bharat.rajoriya11@gmail.com>
2024-10-30 15:23:31 +0530

    Singin_form_created

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import Jury_panel from './Components/Jury_panel'
 import Navbar from './Components/Navbar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Signin from './Components/Signin'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/jury" element={<Jury_panel />} />
           <Route path='/jury:id' element={<Jury_panel />} />
+          <Route path='/' element={<Signin/>}/>
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/Components/Signin.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "../assets/Signin.css"
+
+const Signin = () => {
+  return (
+   <>
+   <div className="singin_container">
+        <div className="singin_form_block">
+        <div className="card_title">
+          <h1>Please login to continue</h1>
+         
+        </div>
+        <div className="form">
+        <form action="/register" method="post">
+          <input type="email" name="email" placeholder="Email" id="email" />
+          <button>Submit</button>
+          </form>
+        </div>
+        </div>
+   </div>
+   </>
+  )
+}
+
+export default Signin
